9285bc8a274fcb3921f4982c86bbef0b1c96bbf9
adrish20 <purkayasthashan@gmail.com>
2024-08-02 23:08:49 +0530

    Add files via upload

---
+++ calculator.cpp
@@ -0,0 +1,29 @@
+//Adrish Purkayastha
+//PRN:23070123011
+//EnTC A1
+#include <iostream>
+using namespace std;
+int main(){
+     float division,a,b,sum,difference,product;//intialising variables
+     cout<<"enter two numbers: ";
+     cin>>a>>b;//take input of two numbers
+     sum = a+b;//calculate sum
+     difference = a-b;//calculate difference
+     product = a*b;//calculate product
+     division = a/b;//calculate division
+     cout<<"sum is: "<<sum<<endl;//display sum
+     cout<<"difference is: "<<difference<<endl;//display difference
+     cout<<"product is: "<<product<<endl;//display product
+     cout<<"division is: "<<division<<endl;//display division
+
+}
+
+/*Output:
+enter two numbers: 2 5
+sum is: 7
+difference is: -3
+product is: 10
+division is: 0.4
+*/
+
+   
---
+++ helloworld.cpp
@@ -0,0 +1,13 @@
+//Adrish Purkayastha
+//PRN:23070123011
+//EnTC A
+//Experiment 1.a
+#include<iostream>
+using namespace std;
+int main(){
+    cout<<"Hello,World!";
+}
+
+/*Output:
+Hello,World!
+ */
